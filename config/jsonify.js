29cc01cc27d32a6402a42d06b9e710ed24bc1812
mikehwagz <mike@selfaware.studio>
2020-03-16 10:01:19 -0400

    create jsonify script

--- config/jsonify.js
+++ config/jsonify.js
@@ -2,11 +2,7 @@ const fs = require('fs')
 const sast = require('sast')
 const visit = require('unist-util-visit')
 const find = require('unist-util-find')
-
-// const contents = fs.readFileSync('src/definitions/_bg.scss', 'utf8')
-// const tree = sast.parse(contents, { syntax: 'scss' })
-// let found = find(tree, { type: 'atkeyword' })
-// log(found, { depth: null })
+const select = require('unist-util-select')
 
 const OUTPUT = []
 
@@ -33,51 +29,79 @@ visit(indexTree, 'declaration', declaration => {
       const tree = sast.parse(contents, { syntax: 'scss' })
       const configurable = !!find(tree, { type: 'atkeyword' })
       if (configurable) {
-        // let each = {
-        //   classname: 'bc-<key>',
-        //   declarations: [
-        //     {
-        //       property: 'border-color',
-        //       propertyValue: '<value>'
-        //     }
-        //   ]
+        switch (name) {
+          case 'ratio':
+            handleNonConfigurableDefitions(rules, tree, name)
 
-        visit(tree, 'block', b => {
-          visit(b, 'declaration', d => {
-            visit(d, 'value', v => {
-              visit(v, 'string', s => {
-                console.log(s.value)
-              })
-            })
-          })
-        })
+            const ratioMaps = select.selectAll('map', tree)
+            let [classname, declaration] = Object.entries(
+              sast.jsonify(ratioMaps[3])
+            )[0]
 
-        // }
-      } else {
-        visit(tree, 'declaration', d => {
-          let entries = Object.entries(sast.jsonify(d).value)
-
-          for (let n = 0; n < entries.length; n++) {
-            let [key, val] = entries[n]
-            let classname = key.replace(/\'/g, '')
-            let declarations = Object.entries(val).map(
-              ([property, propertyValue]) => {
-                return {
-                  property,
-                  propertyValue:
-                    typeof propertyValue === 'string' &&
-                    propertyValue.includes('unquote')
-                      ? propertyValue.slice(`unquote('`.length, -2)
-                      : propertyValue
-                }
-              }
-            )
             rules.push({
-              classname,
-              declarations
+              classname: handleInterpolatedVariable(classname),
+              declarations: Object.entries(declaration).map(
+                ([property, propertyValue]) => {
+                  return {
+                    property,
+                    propertyValue:
+                      propertyValue
+                        .split('$')
+                        .join('<')
+                        .slice(0, -1) + '>)'
+                  }
+                }
+              )
             })
-          }
-        })
+
+            break
+          case 'margin':
+            handleNonConfigurableDefitions(rules, tree, name)
+
+            const marginMaps = select.selectAll('map', tree)
+            const marginEntries = Object.entries(sast.jsonify(marginMaps[8]))
+
+            for (let i = 0; i < marginEntries.length; i++) {
+              let [key, val] = marginEntries[i]
+              rules.push({
+                classname: handleInterpolatedVariable(key),
+                declarations: Object.entries(val).map(
+                  ([property, propertyValue]) => {
+                    return {
+                      property,
+                      propertyValue: propertyValue.split('$').join('<') + '>'
+                    }
+                  }
+                )
+              })
+            }
+
+            break
+          case 'padding':
+            const paddingMap = select.select('map', tree)
+            const paddingEntries = Object.entries(sast.jsonify(paddingMap))
+
+            for (let i = 0; i < paddingEntries.length; i++) {
+              let [key, val] = paddingEntries[i]
+              rules.push({
+                classname: handleInterpolatedVariable(key),
+                declarations: Object.entries(val).map(
+                  ([property, propertyValue]) => {
+                    return {
+                      property,
+                      propertyValue: propertyValue.split('$').join('<') + '>'
+                    }
+                  }
+                )
+              })
+            }
+
+            break
+          default:
+            handleDefaultConfigurableDefinitions(rules, tree, name)
+        }
+      } else {
+        handleNonConfigurableDefitions(rules, tree, name)
       }
 
       OUTPUT.push({
@@ -90,11 +114,63 @@ visit(indexTree, 'declaration', declaration => {
   }
 })
 
-// log(OUTPUT)
 fs.writeFileSync('config/output.json', JSON.stringify(OUTPUT, null, 2))
 
-// helper
+function handleNonConfigurableDefitions(rules, tree, name) {
+  let map = select.select('declaration value map', tree)
+  let entries = Object.entries(sast.jsonify(map))
+  for (let n = 0; n < entries.length; n++) {
+    let [key, val] = entries[n]
+    let classname = key.replace(/\'/g, '')
+    let declarations = Object.entries(val).map(([property, propertyValue]) => {
+      return {
+        property,
+        propertyValue:
+          typeof propertyValue === 'string' && propertyValue.includes('unquote')
+            ? propertyValue.slice(`unquote('`.length, -2)
+            : propertyValue
+      }
+    })
+    rules.push({
+      classname,
+      declarations
+    })
+  }
+}
+
+function handleDefaultConfigurableDefinitions(rules, tree, name) {
+  rules.push({
+    classname: handleInterpolatedVariable(
+      select.select('block declaration value string', tree).value
+    ),
+    declarations: Object.entries(
+      sast.jsonify(select.select('block declaration map map', tree))
+    ).map(([property, propertyValue]) => {
+      if (propertyValue.includes('unquote')) {
+        propertyValue = propertyValue.slice(`unquote(`.length, -1)
+      }
+
+      if (propertyValue.includes('$')) {
+        propertyValue = `<${propertyValue.slice(1)}>`
+      }
+
+      return {
+        property,
+        propertyValue
+      }
+    })
+  })
+}
+
+function handleInterpolatedVariable(string) {
+  return string
+    .slice(1, -1)
+    .split('#{$')
+    .join('<')
+    .split('}')
+    .join('>')
+}
 
 function log(x) {
-  console.dir(x, { depth: null })
+  console.log(JSON.stringify(x, null, 2))
 }
